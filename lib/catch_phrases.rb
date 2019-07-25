cf09ab7963380d92dc87395e70716fd687d98294
Israel Sanchez <israel.pro1994@gmail.com>
2019-07-25 01:26:01 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,11 +1,12 @@
 def mario
   phrase = "It's-a me, Mario!"
-  
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
   phrase
 end
 
 
 def toadstool
+  
   puts status
 end
